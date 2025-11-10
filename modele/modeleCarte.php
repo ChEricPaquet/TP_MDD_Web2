04897980a2f5f1f1c8ed15e4d6427fb6e43b9afc
ChEricPaquet <2437295@cegepba.qc.ca>
2025-11-10 09:06:29 -0500

    Creation function modele carte

---
+++ modele/modeleCarte.php
@@ -0,0 +1,36 @@
+<?php
+require_once "modele/bd.php";
+
+class ModeleCarte
+{
+    public static function ObtenirParId($id)
+    {
+        $connection = BD::ObtenirConnexion();
+
+        $req = $connection->prepare(
+            "SELECT * FROM carte WHERE id_carte = :id_carte"
+        );
+
+        $req->bindParam('id_carte', $id);
+
+        $req->execute();
+
+        return $req;
+    }
+
+    public static function ObtenirTout()
+    {
+                $connection = BD::ObtenirConnexion();
+
+        $req = $connection->prepare(
+            "SELECT * FROM carte"
+        );
+
+        $req->bindParam('id_carte', $id);
+
+        $req->execute();
+
+        return $req;
+    }
+}
+?>
