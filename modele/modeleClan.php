1f69a97524eea91db58e6aa6e7f4dc63492a980f
ChEricPaquet <2437295@cegepba.qc.ca>
2025-11-10 09:51:29 -0500

    Ajout et supprimer modele clan

--- modele/modeleClan.php
+++ modele/modeleClan.php
@@ -1,4 +1,33 @@
 <?php
 require_once "modele/bd.php";
 
+class modeleClan
+{
+    public static function ajouterUtilisateurClan($idUtilisateur, $idClan)
+    {
+        $connexion = BD::ObtenirConnexion();
+
+        $req = $connexion->prepare(
+            "INSERT INTO UtilisateurClan (id_Utilisateur, id_Clan) VALUES (id_Utilisateur :idUtilisateur,id_Clan :idClan)"
+        );
+
+        $req->bindParam(':idUtilisateur', $idUtilisateur);
+        $req->bindParam(':idClan', $idClan);
+
+        $req->execute();
+
+        return $connexion->lastInsertId();
+    }
+
+        public static function suprimerUtilisateurClan($idUtilisateur, $idClan)
+    {
+        $connexion = BD::ObtenirConnexion();
+       
+        $req = $connexion->prepare(
+            "DELETE FROM clan WHERE idClan = :idClan"
+        );
+
+        $req->execute();
+    }
+}
 ?>
