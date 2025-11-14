92afc11892508d31c46df05cdd5b9a11a2d9b380
ChEricPaquet <2437295@cegepba.qc.ca>
2025-11-14 08:20:04 -0500

    Insert utilisateur

--- bd.sql
+++ bd.sql
@@ -192,3 +192,15 @@ VALUES
 ('Zap','Zap.png','Sort qui inflige des dégâts électriques de zone et étourdit les ennemis', 1), 
 ('Zappy','Zappy.png','Bâtiment qui tire des éclairs sur les ennemis proches', 2)
 
+INSERT INTO Utilisateurs (nom, mot_de_passe)
+VALUES
+('Alice Tremblay', 'At!92xQm7'),
+('Benoît Gagnon', 'Bg#74LmZp'),
+('Clara Dubois', 'Cd*58VrNt'),
+('David Lavoie', 'Dl@63QwXe'),
+('Élodie Fortin', 'Ef!81ZkRt'),
+('François Moreau', 'Fm#97YtLp'),
+('Gabrielle Roy', 'Gr*46PwMn'),
+('Hugo Boucher', 'Hb@55KsQz'),
+('Isabelle Caron', 'Ic!72XvJr'),
+('Julien Martel', 'Jm#83NtWq')	
