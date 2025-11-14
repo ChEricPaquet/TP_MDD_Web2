0d2429f7a26e569c6e81f18bd21160cd27a342d4
JustinLafontain3 <2436505@cegepba.qc.ca>
2025-11-14 09:48:35 -0500

    Ajouter commentaire

--- modele/modeleCommentaire.php
+++ modele/modeleCommentaire.php
@@ -1,4 +1,23 @@
 <?php
 require_once "modele/bd.php";
 
+class modeleCommentaire{
+    public static function AjouterCommentaire($id_Utilisateur,$texte,$id_Deck){
+        $connexion = BD::ObtenirConnexion();
+
+        $req = $connexion->prepare(
+                "INSERT INTO Commentaire (id_utilisateur, texte, id_deck) VALUES (id_utilisateur :idUtilisateur, texte :texte, id_deck :idDeck)"
+        );
+
+        $req->bindParam(':idUtilisateur', $id_Utilisateur);
+        $req->bindParam(':texte', $texte);
+        $req->bindParam(':idDeck', $id_Deck);
+        
+
+
+        $req->execute();
+
+        return $connexion->lastInsertId();
+    }
+}
 ?>
